7838a732ca1d3787a9c64582cb582661ac5d9475
Panguah <guilherme.peixoto.mourao@gmail.com>
2021-09-28 02:59:40 -0300

    adding Daily LE

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
-// Function that converts the LE and add the div to each item.
-function addLE(dataName, result) {
-   // Remove all the old converted LE from the previous page.
-   document.querySelectorAll('.converted_LE').forEach(e => e.remove());
+function deleteOld () {
+   document.querySelectorAll('.daily_LE').forEach(e => e.remove());
+   document.querySelectorAll('.hourly_LE').forEach(e => e.remove());
+}
+
+// Function that converts and add info to hourly LE
+function addHourlyLE(dataName) {
    // Converting the value and preparing it to insert in the div.
    var elems = document.querySelectorAll('[' + dataName + ']');
    var index = 0, length = elems.length;
@@ -16,8 +19,36 @@ function addLE(dataName, result) {
       var numFinal = numOp.toFixed(1)
       // Creating, styling and positioning the div with the converted LE.
       var div = document.createElement('div')
-      div.innerHTML = numFinal + result
-      div.setAttribute('class', 'converted_LE')
+      div.innerHTML = numFinal + '/Hour'
+      div.setAttribute('class', 'hourly_LE')
+      div.style.color = '#fff';
+      div.style.border = '1px solid #ccc';
+      div.style.borderRadius = '5px';
+      div.style.padding = '3px'
+      div.style.margin = '10px'
+      div.style.textAlign = 'center'
+      elems[index].prepend(div)
+   }
+}
+// Function that converts and add info to daily LE
+function addDailyLE(dataName) {
+   // Converting the value and preparing it to insert in the div.
+   var elems = document.querySelectorAll('[' + dataName + ']');
+   var index = 0, length = elems.length;
+   for (; index < length; index++) {
+      var num = elems[index].innerHTML.match(/\d+/g).join('/').substring(9);
+      var numOp = eval(num);
+      // For some reason the node array we get from the elems comes
+      // with some undefined strings. This is to get rid of them.
+      if (numOp === undefined) {
+         continue;
+      }
+      numOp * 24
+      var numFinal = numOp.toFixed(1)
+      // Creating, styling and positioning the div with the converted LE.
+      var div = document.createElement('div')
+      div.innerHTML = numFinal + '/Day'
+      div.setAttribute('class', 'daily_LE')
       div.style.color = '#fff';
       div.style.border = '1px solid #ccc';
       div.style.borderRadius = '5px';
@@ -32,7 +63,9 @@ function detectPageChange() {
    var currentPage = document.querySelector('p.currentPage');
    var observer = new MutationObserver(function () {
       setTimeout(function () {
-         addLE('data-v-4b7e9996', '/Hour');
+         deleteOld();
+         addHourlyLE('data-v-4b7e9996');
+         addDailyLE('data-v-4b7e9996');
       }, 1000)
    })
    observer.observe(currentPage, { subtree: true, characterData: true })
@@ -45,7 +78,9 @@ function checkLoop() {
          checkLoop()
       }
       else {
-         addLE('data-v-4b7e9996', '/Hour');
+         deleteOld();
+         addHourlyLE('data-v-4b7e9996');
+         addDailyLE('data-v-4b7e9996');
          detectPageChange();
       }
    }, 1000)
